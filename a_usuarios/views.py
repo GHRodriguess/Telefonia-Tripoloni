3da8e2e3845f0bd2523f8cb1895eab5bffb5d9df
Gabriel GHR. Henrique Rodrigues <gabrielrodrigues@tripoloni.com>
2025-05-06 17:50:39 -0300

    inicio das configurações testando conexão com ad

--- a_usuarios/views.py
+++ a_usuarios/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from utils.global_utils import  check_permission
+from django.contrib.auth import login, logout, authenticate
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-def login(request):
+def login_view(request):    
     return render(request, "login.html")
+
+@check_permission(permissions=['login_required'])
+def logout_view(request):
+    logout(request)
+    return redirect('hub')
