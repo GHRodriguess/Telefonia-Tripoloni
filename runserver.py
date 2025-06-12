031b19ac36d29c664dcb2c6e0c210e9404d6dbde
Gabriel GHR. Henrique Rodrigues <gabrielrodrigues@tripoloni.com>
2025-06-12 15:56:32 -0300

    alterando o requirements.txt e adicionando o runserver.py

---
+++ runserver.py
@@ -0,0 +1,6 @@
+from waitress import serve
+from p_telefonia_tripoloni.wsgi import application  
+
+if __name__ == '__main__':
+    print("Iniciando servidor com Waitress em http://0.0.0.0:8000 ...")
+    serve(application, host='0.0.0.0', port=8000, _quiet=False)
