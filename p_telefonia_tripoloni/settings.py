3d3b638e91bce2382704cfbf848d4cca0a0a778f
Gabriel GHR. Henrique Rodrigues <gabrielrodrigues@tripoloni.com>
2025-06-12 16:02:31 -0300

    adicionando o whitenoise no middleware e nas instalações

--- p_telefonia_tripoloni/settings.py
+++ p_telefonia_tripoloni/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'p_telefonia_tripoloni.urls'
