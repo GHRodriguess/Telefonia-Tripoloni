41a98379b1260523cef5c51694b538bb521b786a
Gabriel GHR. Henrique Rodrigues <gabrielrodrigues@tripoloni.com>
2025-06-12 17:48:40 -0300

    adicionando commit automatico quando rodar collectstatic

--- a_home/apps.py
+++ a_home/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 import sys
+import subprocess
 from django.core.management import call_command
 
 class AHomeConfig(AppConfig):
@@ -11,5 +12,11 @@ class AHomeConfig(AppConfig):
             try:
                 call_command('collectstatic', interactive=False, verbosity=0)
                 print("✅ Arquivos estáticos coletados automaticamente.")
+                subprocess.run(['git', 'add', 'staticfiles/'], check=True)
+                subprocess.run(['git', 'commit', '-m', 'Atualizando os arquivos estáticos após collectstatic (automático)'])
+                subprocess.run(['git', 'push'], check=True)
+                print("✅ Commit automático feito com sucesso.")
+            except subprocess.CalledProcessError as e:
+                print("❌ Erro ao executar comando Git:", e)
             except Exception as e:
                 print("❌ Erro ao rodar collectstatic automaticamente:", e)
